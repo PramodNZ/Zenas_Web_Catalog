e02070511add03ffdcaf24aca79828a2afbedeef
PramodNZ <152771728+PramodNZ@users.noreply.github.com>
2024-05-13 13:02:24 +1200

    Update zenas_web_Catalog.py

--- zenas_web_Catalog.py
+++ zenas_web_Catalog.py
@@ -15,7 +15,7 @@ st.write("The name on your order will be:", name_on_order)
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),
-CURRENT_REGION()")
+# CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 st.text("Hello from Snowflake:")
 st.text(my_data_row)
