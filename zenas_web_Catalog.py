ca8e7f919ce8de1639aeb678bd7db71005b930b8
PramodNZ <152771728+PramodNZ@users.noreply.github.com>
2024-05-13 12:55:01 +1200

    Update zenas_web_Catalog.py

--- zenas_web_Catalog.py
+++ zenas_web_Catalog.py
@@ -12,14 +12,14 @@ st.write(
 name_on_order = st.text_input("Name on Order:")
 st.write("The name on your order will be:", name_on_order)
 
-my_cnx = st.connection("snowflake")
-session = my_cnx.session()
+# my_cnx = st.connection("snowflake")
+# session = my_cnx.session()
 # my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
 
-# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-# my_cur = my_cnx.cursor()
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),
 # CURRENT_REGION()")
 # my_data_row = my_cur.fetchone()
